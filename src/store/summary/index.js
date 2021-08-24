fb0cc32b3a059e94eadff9e8b7409323de7f475b
Mobbbb <491450147@qq.com>
2021-08-24 09:38:59 +0800

    总结页面增加接口请求

--- src/store/summary/index.js
+++ src/store/summary/index.js
@@ -1,4 +1,4 @@
-import { fetchMovieListData } from '@/api/movie.js'
+import { fetchSummaryListData } from '@/api/summary.js'
 import { initSummaryListData } from '@/libs/data-processing'
 
 const summary = {
@@ -10,26 +10,7 @@ const summary = {
         }
     },
     getters: {
-        showMovieList(state, getters, rootState) {
-            if (rootState.app.searchFlag) {
-                return state.filterSearchTextData
-            } else {
-                return state.movieList
-            }
-        },
-        filterSelectedStatusConfig(state) {
-            const hasSelectedRateScore = state.selectedFilter.rateScore[0] !== 0 
-            || state.selectedFilter.rateScore[1] !== movieTotalScore
-
-            return {
-                hasSelectedRateScore,
-            }
-        },
-        hasSelectedFilter(state, getters) {
-            return Object.keys(getters.filterSelectedStatusConfig)
-                    .filter(key => getters.filterSelectedStatusConfig[key])
-                    .length
-        },
+        
     },
     mutations: {
         setArticleList(state, value) {
@@ -44,31 +25,10 @@ const summary = {
             if (state.articleList.length) return 
 
             commit('setLoadingStatus', true)
-            // const { data: listData } = await fetchMovieListData()
-            let listData = [
-                {
-                    title: '这里是写总结的地方',
-                    label: ['HTML'],
-                    content: [
-                        {
-                            type: 'text',
-                            value: '如有侵权或错误处，可在github.com/Mobbbb联系我。If there is any infringement or error, please contact me at github.com/mobbbb.                            ',
-                        },
-                        {
-                            type: 'img',
-                            value: '',
-                        },
-                        {
-                            type: 'code',
-                            value: '',
-                        },
-                    ],
-                    date: '2021-06-06',
-                }
-            ]
-            initSummaryListData(listData)
+            const { data: listData = [] } = await fetchSummaryListData()
             commit('setLoadingStatus', false)
 
+            initSummaryListData(listData)
             commit('setArticleList', listData)
         },
     },
